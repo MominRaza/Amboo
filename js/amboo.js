1ac150651c17d542a107a818e6233cdfe68d2a93
MominRaza <mominr082@gmail.com>
2021-06-15 16:10:26 +0530

    added modal and form

--- js/amboo.js
+++ js/amboo.js
@@ -1,3 +1,7 @@
 function toggleMenu() {
-    document.body.classList.toggle('menu-active');
+  document.body.classList.toggle("menu-active");
+}
+function toggleModal(target) {
+  modal = document.getElementById(target);
+  modal.classList.toggle("modal-active");
 }
