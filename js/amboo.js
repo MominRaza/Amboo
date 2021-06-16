7338c0b1674d21f2af9e686d562c8c07d2bbc5ba
MominRaza <mominr082@gmail.com>
2021-06-16 23:02:38 +0530

    fixed icon with fab and btn

--- js/amboo.js
+++ js/amboo.js
@@ -1,10 +1,12 @@
 function toggleMenu() {
   document.body.classList.toggle("menu-active");
 }
+
 function toggleModal(target) {
   modal = document.getElementById(target);
   modal.classList.toggle("modal-active");
 }
+
 function toggleSubMenu(subMenu) {
   subMenu.classList.toggle("submenu-active");
 }
